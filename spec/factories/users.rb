b7520da07d5b5052ba4442773b3ad351bc9ff5df
Zachary Cutler <zachlcutler@gmail.com>
2016-10-04 15:56:15 -0400

    progress commit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
-  factory :user do
-    
-  end
+ factory :user do
+   first_name "Chris"
+   last_name "Serpa"
+   email "cserpa123@gmail.com"
+   encrypted_password "devise"
+ end
 end
