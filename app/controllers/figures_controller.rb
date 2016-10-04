7b296a302939a88cb2b3cbf47b39e59c635d1482
cameroncogan <camycogan@gmail.com>
2016-10-04 14:19:15 -0400

    added figure creation test and functionality

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,5 +10,25 @@ class FiguresController < ApplicationController
     @ratings = Rating.where(figure_id: @figure.id)
   end
 
+  def new
+    @figure = Figure.new
+  end
 
+  def create
+    @figure = Figure.new(figure_params)
+
+    if @figure.save
+      flash[:notice] = "Figure added successfully"
+      redirect_to figure_path(@figure)
+    else
+      flash[:notice] = @figure.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  private
+
+  def figure_params
+    params.require(:figure).permit(:name, :occupation, :era, :nationality, :claim_to_fame)
+  end  
 end
