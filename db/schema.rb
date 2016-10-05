bc4a061d90dc977c6ac8b09ef2b102ec87f16196
cserpa <cserpa123@gmail.com>
2016-10-05 16:06:07 -0400

    added rating and review functionality, added average rating to figure model, removed average rating from schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005154608) do
+ActiveRecord::Schema.define(version: 20161005200232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +31,11 @@ ActiveRecord::Schema.define(version: 20161005154608) do
   end
 
   create_table "figures", force: :cascade do |t|
-    t.string   "name",           null: false
-    t.string   "occupation",     null: false
-    t.string   "era",            null: false
-    t.string   "nationality",    null: false
-    t.string   "claim_to_fame",  null: false
-    t.float    "average_rating"
+    t.string   "name",          null: false
+    t.string   "occupation",    null: false
+    t.string   "era",           null: false
+    t.string   "nationality",   null: false
+    t.string   "claim_to_fame", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
