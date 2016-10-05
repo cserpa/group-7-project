1adc7cbecebd8ffc1db7d2296ad77cfb84e9da34
mfolson55 <mylesolson55@gmail.com>
2016-10-05 09:42:15 -0400

    fixed spacing issues

--- spec/features/03_user_goes_to_category_page_spec.rb
+++ spec/features/03_user_goes_to_category_page_spec.rb
@@ -1,28 +1,36 @@
 require 'rails_helper'
 
 feature 'visitor goes to category show page' do
-  let!(:myles) { User.create!(first_name: 'Myles', last_name: 'Olson',
+  let!(:myles) { User.create!(first_name: 'Myles',
+                              last_name: 'Olson',
                               email: 'myles@example.com',
                               password: 'password') }
-  let!(:cameron) { User.create!(first_name: 'Cameron', last_name: 'Cogan',
+  let!(:cameron) { User.create!(first_name: 'Cameron',
+                                last_name: 'Cogan',
                                 email: 'cameron@example.com',
                                 password: 'password') }
-  let!(:hitler) { Figure.create!(name: 'Adolf Hitler', occupation: 'Dictator',
-                                 era: "1900's", nationality: 'Austrian',
+  let!(:hitler) { Figure.create!(name: 'Adolf Hitler',
+                                 occupation: 'Dictator',
+                                 era: "1900's",
+                                 nationality: 'Austrian',
                                  claim_to_fame:'Killed six million people',
                                  average_rating: 0) }
   let!(:lincoln) { Figure.create!(name: 'Abraham Lincoln',
-                                  occupation: 'Politician', era: "1800's",
+                                  occupation: 'Politician',
+                                  era: "1800's",
                                   nationality: 'American',
                                   claim_to_fame: 'Ended slavery',
                                   average_rating: 5) }
   let!(:rating_one) { Rating.create!(rating: 5,
                                      review: 'I love Lincoln!'
-                                     user_id: myles.id, figure_id: lincoln.id) }
-  let!(:rating_two) { Rating.create!(rating: 0, review: 'Hitler sucks',
-                                     user_id: myles.id, figure_id: hitler.id) }
+                                     user_id: myles.id,
+                                     figure_id: lincoln.id) }
+  let!(:rating_two) { Rating.create!(rating: 0,
+                                     review: 'Hitler sucks',
+                                     user_id: myles.id,
+                                     figure_id: hitler.id) }
   let!(:politician) { Category.create!(name: 'Politicians') }
-  let!(:lincoln_politician) { Categorization.create!( figure_id: lincoln.id,
+  let!(:lincoln_politician) { Categorization.create!(figure_id: lincoln.id,
                                                   category_id: politician.id ) }
   let!(:hitler_politician) { Categorization.create!( figure_id: hitler.id,
                                                   category_id: politician.id ) }
