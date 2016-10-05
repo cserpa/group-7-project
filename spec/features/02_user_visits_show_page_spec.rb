4302833cb0c64a1be7d17bffcd05e59d333cba20
cameroncogan <camycogan@gmail.com>
2016-10-05 09:51:14 -0400

    more fixes

--- spec/features/02_user_visits_show_page_spec.rb
+++ spec/features/02_user_visits_show_page_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
 feature 'visitors see profile and reviews on show page' do
-  let!(:myles) { User.create!(first_name: 'Myles', last_name: 'Olson', email: 'myles@example.com', password: 'password') }
-  let!(:cameron) { User.create!(first_name: 'Cameron', last_name: 'Cogan', email: 'cameron@example.com', password: 'password') }
-  let!(:lincoln) { Figure.create!(name: 'Abraham Lincoln', occupation: 'Politician', era: "1800's", nationality: 'American', claim_to_fame: 'Ended slavery', average_rating: 5) }
-  let!(:rating_one) { Rating.create!(rating: 5, review: 'I love Lincoln, this dude was bomb. I am being him for Halloween.', user_id: myles.id, figure_id: lincoln.id) }
+  let!(:myles) {
+    User.create!(first_name: 'Myles',
+      last_name: 'Olson',
+      email: 'myles@example.com',
+      password: 'password')
+    }
+  let!(:cameron) {
+    User.create!(first_name: 'Cameron',
+      last_name: 'Cogan',
+      email: 'cameron@example.com',
+      password: 'password')
+    }
+  let!(:lincoln) {
+    Figure.create!(name: 'Abraham Lincoln',
+      occupation: 'Politician',
+      era: "1800's",
+      nationality: 'American',
+      claim_to_fame: 'Ended slavery',
+      average_rating: 5)
+    }
+  let!(:rating_one) {
+    Rating.create!(rating: 5,
+      review: 'I love Lincoln, this dude was bomb. I am being him for Halloween.',
+      user_id: myles.id,
+      figure_id: lincoln.id)
+    }
 
   scenario 'show page has profile info' do
     visit figure_path(lincoln)
@@ -13,7 +35,7 @@ feature 'visitors see profile and reviews on show page' do
     expect(page).to have_content 'Occupation: Politician'
     expect(page).to have_content "Era: 1800's"
     expect(page).to have_content 'Nationality: American'
-    expect(page).to have_content '[Claim to fame: Ended slavery'
+    expect(page).to have_content 'Claim to fame: Ended slavery'
 
   end
 
