93f8fdf87692b13da131f941d9196cccf779db1f
Zachary Cutler <zachlcutler@gmail.com>
2016-10-05 10:58:02 -0400

    progress commit-hound violations

--- spec/features/user_signs_into_existing_account_spec.rb
+++ spec/features/user_signs_into_existing_account_spec.rb
@@ -1,9 +1,14 @@
-require 'spec_helper'
-require "rails_helper"
 # frozen_string_literal: true
 
+require 'spec_helper'
+require "rails_helper"
+
 feature 'user can log in/out of existing account' do
-  let!(:user) { User.create!(email: 'cameron@example.com', first_name: 'Cameron', last_name: 'Cogan', password: 'password') }
+  let!(:user) do User.create!(email: 'cameron@example.com',
+    first_name: 'Cameron',
+    last_name: 'Cogan',
+    password: 'password')
+  end
 
   scenario 'successful sign-in of inauthenticated with valid information' do
     visit root_path
@@ -15,14 +20,14 @@ feature 'user can log in/out of existing account' do
     expect(page).to have_content('Signed in successfully.')
   end
 
-  scenario 'unsuccessful sign-in of inautheticated user with invalid password' do
+  scenario 'unsuccessful sign-in of inautheticated user with wrong password' do
     visit root_path
     click_link 'Sign In'
     fill_in "Email", with: user.email
     fill_in 'Password', with: 'Wrong'
     click_button 'Log in'
 
-    expect(page).to have_content("Invalid Email or password.")
+    expect(page).to have_content('Invalid Email or password.')
   end
 
   scenario 'successful sign-out of authenticated user' do
