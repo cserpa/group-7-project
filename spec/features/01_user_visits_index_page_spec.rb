2f4231df5842e775f5987e70f2a8c75f24576d2d
cserpa <cserpa123@gmail.com>
2016-10-05 09:53:08 -0400

    edited spec_01 again

--- spec/features/01_user_visits_index_page_spec.rb
+++ spec/features/01_user_visits_index_page_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 feature 'visitors see the index page' do
-  let!(:lincoln) { Figure.create!(name: 'Abraham Lincoln', occupation: 'politician', era: '1800s', nationality: 'American', claim_to_fame: 'Ended slavery', average_rating: 5) }
+  let!(:lincoln) {
+    Figure.create!(name: 'Abraham Lincoln',
+    occupation: 'politician',
+    era: '1800s',
+    nationality: 'American',
+    claim_to_fame: 'Ended slavery',
+    average_rating: 5)
+  }
 
   scenario 'visitor visits the index page' do
     visit figures_path
@@ -9,7 +16,7 @@ feature 'visitors see the index page' do
     expect(page).to have_content 'Abraham Lincoln - politician'
   end
 
-  scenario 'clicking on a figure will take the user to the Figure\'s show page' do
+  scenario 'clicking figure will take the user to the Figure\'s show page' do
     visit figures_path
     click_link 'Abraham Lincoln'
     expect(current_path).to eq figure_path(lincoln)
