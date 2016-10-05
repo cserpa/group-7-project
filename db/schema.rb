bf287052ae5f344e7294f346fc59d971b2267b9b
cameroncogan <camycogan@gmail.com>
2016-10-05 11:48:40 -0400

    removes vote table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004170706) do
+ActiveRecord::Schema.define(version: 20161005154608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,15 +69,6 @@ ActiveRecord::Schema.define(version: 20161004170706) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "rating_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["rating_id"], name: "index_votes_on_rating_id", using: :btree
-    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
   end
 
 end
