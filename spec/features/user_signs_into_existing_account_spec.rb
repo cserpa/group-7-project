27375810c0776c9d613cf248938837d852c3b33c
Zachary Cutler <zachlcutler@gmail.com>
2016-10-05 10:52:29 -0400

    fixed hound violations in user sign-in specs

--- spec/features/user_signs_into_existing_account_spec.rb
+++ spec/features/user_signs_into_existing_account_spec.rb
@@ -2,37 +2,37 @@ require 'spec_helper'
 require "rails_helper"
 
 
-feature "user can log in/out of existing account" do
-  let!(:user) { User.create!(email: "cameron@example.com", first_name: 'Cameron', last_name: 'Cogan', password: 'password') }
+feature 'user can log in/out of existing account' do
+  let!(:user) { User.create!(email: 'cameron@example.com', first_name: 'Cameron', last_name: 'Cogan', password: 'password') }
 
-  scenario "successful sign-in of inauthenticated with valid information" do
+  scenario 'successful sign-in of inauthenticated with valid information' do
     visit root_path
     click_link 'Sign In'
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
 
-    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content('Signed in successfully.')
   end
 
-  scenario "unsuccessful sign-in of inautheticated user with invalid password" do
+  scenario 'unsuccessful sign-in of inautheticated user with invalid password' do
     visit root_path
     click_link 'Sign In'
     fill_in "Email", with: user.email
-    fill_in "Password", with: "Wrong"
-    click_button "Log in"
+    fill_in 'Password', with: 'Wrong'
+    click_button 'Log in'
 
     expect(page).to have_content("Invalid Email or password.")
   end
 
-  scenario "successful sign-out of authenticated user" do
+  scenario 'successful sign-out of authenticated user' do
     visit root_path
     click_link 'Sign In'
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    click_link "Sign Out"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    click_link 'Sign Out'
 
-    expect(page).to have_content("Signed out successfully.")
+    expect(page).to have_content('Signed out successfully.')
   end
 end
