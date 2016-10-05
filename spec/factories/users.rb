40711be56e8908d14fa35eda6bc5eae73c9990a5
cserpa <cserpa123@gmail.com>
2016-10-05 16:16:15 -0400

    edited factory for hound

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 FactoryGirl.define do
  factory :user do
-   first_name "Chris"
-   last_name "Serpa"
-   email "cserpa123@gmail.com"
-   password "devise"
+   first_name 'Chris'
+   last_name 'Serpa'
+   email 'cserpa123@gmail.com'
+   password 'devise'
  end
 
  factory :figure do
-   name "Abraham Lincoln"
-   occupation "President"
-   nationality "American"
-   era "1800s"
-   claim_to_fame "Freed the slaves"
+   name 'Abraham Lincoln'
+   occupation 'President'
+   nationality 'American'
+   era '1800s'
+   claim_to_fame 'Freed the slaves'
  end
 end
