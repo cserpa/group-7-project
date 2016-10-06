3231975e3236e237b4f8615a5051af2121c95da9
cserpa <cserpa123@gmail.com>
2016-10-06 16:49:14 -0400

    wrote tests

---
+++ spec/features/09_user_can_vote_on_reviews_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'rails_helper'
+
+# [] User should be able to see score of a review (upvotes - downvotes)
+# [] User can upvote or downvote a review
+# [] User cannot upvote or downvote rating
+# [] User can only upvote or downvote a review once
+# [] User can change from upvote to downvote and vice versa
+
+feature 'user can upvote or downvote a review' do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:figure) { FactoryGirl.create(:figure) }
+  let!(:rating) { FactoryGirl.create(:rating, user_id: user.id, figure_id: figure.id) }
+
+  scenario 'user upvotes a review' do
+    sign_in(user)
+    visit figure_path(figure)
+
+    click_button 'Upvote'
+
+    expect(page).to have_content 'Upvoted!'
+    expect(page).to have_content '1'
+  end
+
+  scenario 'user downvotes a review' do
+    sign_in(user)
+    visit figure_path(figure)
+
+    click_button 'Downvote'
+
+    expect(page).to have_content 'Downvoted!'
+    expect(page).to have_content '-1'
+  end
+
+  scenario 'user upvotes a review and tries to upvote again' do
+    sign_in(user)
+    visit figure_path(figure)
+
+    click_button 'Upvote'
+    click_button 'Upvote'
+
+    expect(page).to have_content 'You may only upvote or downvote a review once'
+    expect(page).to have_content '1'
+  end
+
+  scenario 'user upvotes a review and then changes to downvote' do
+    sign_in(user)
+    visit figure_path(figure)
+
+    click_button 'Upvote'
+
+    expect(page).to have_content 'Upvoted!'
+    expect(page).to have_content '1'
+
+    click_button 'Downvote'
+
+    expect(page).to have_content 'Downvoted!'
+    expect(page).to have_content '0'
+  end
+end
