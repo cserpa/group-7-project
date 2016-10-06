c10109eb38a3025057bc0d4fc821a875e0d66e2e
cserpa <cserpa123@gmail.com>
2016-10-06 11:08:41 -0400

    Removed frozen string literal from every file, added .rubocop.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 class User < ActiveRecord::Base
   has_many :ratings
   has_many :votes
--- db/migrate/20161004140634_add_figures.rb
+++ db/migrate/20161004140634_add_figures.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 class AddFigures < ActiveRecord::Migration[5.0]
   def change
     create_table :figures do |t|
@@ -7,6 +6,6 @@ class AddFigures < ActiveRecord::Migration[5.0]
       t.string :era, null: false
       t.string :nationality, null: false
       t.string :claim_to_fame, null: false
-    end  
+    end
   end
 end
