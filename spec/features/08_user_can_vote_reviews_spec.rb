8e0fcd6039b40330b7a03e769f28d3cfcd99d4f3
cserpa <cserpa123@gmail.com>
2016-10-06 14:51:18 -0400

    hound?

---
+++ spec/features/08_user_can_vote_reviews_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require 'rails_helper'
+
+# [] User should be to upvote or downvote a review
+# [] User can only upvote or downvote a review once
+# [] User can change from upvote to downvote, and vice versa
+# [] User can see total of upvotes and downvotes
