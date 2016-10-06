751f641980d0dea6c4b8d6ad51424e83cd008f20
cameroncogan <camycogan@gmail.com>
2016-10-06 11:45:03 -0400

    more hound

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
-  
   has_many :ratings
-  has_many :votes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
