c5a981f1cec2f9a9a551aaecd927294439e1d161
Zachary Cutler <zachlcutler@gmail.com>
2016-10-06 13:02:21 -0400

    more hound violations ...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,12 +26,10 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
-
   def user_params
-    params.require(:user).permit(:id, :email, :first_name, :last_name, :avatar, :avatar_cache)
+    params.require(:user).permit(:id, :email, :first_name, :last_name,
+                                                :avatar, :avatar_cache)
   end
-
 end
