1708c7596208f80c0e4be9b9311dcbff7b63c887
cameroncogan <camycogan@gmail.com>
2016-10-06 11:39:51 -0400

    more hound

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
@@ -28,15 +29,15 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   version :large_avatar do
     # returns a 150x150 image
-    process :resize_to_fill => [150, 150]
+    process resize_to_fill: [150, 150]
   end
   version :medium_avatar do
     # returns a 50x50 image
-    process :resize_to_fill => [50, 50]
+    process resize_to_fill: [50, 50]
   end
   version :small_avatar do
     # returns a 35x35 image
-    process :resize_to_fill => [35, 35]
+    process resize_to_fill: [35, 35]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
