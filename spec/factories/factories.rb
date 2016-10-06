692c5c21dee68b8821b9d5a3a4e8e58e40df3e3e
cserpa <cserpa123@gmail.com>
2016-10-06 14:16:52 -0400

    fixed factory

---
+++ spec/factories/factories.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+ factory :user do
+   first_name 'Chris'
+   last_name 'Serpa'
+   email 'cserpa123@gmail.com'
+   password 'devise'
+ end
+
+ factory :figure do
+   name 'Abraham Lincoln'
+   occupation 'President'
+   nationality 'American'
+   era '1800s'
+   claim_to_fame 'Freed the slaves'
+ end
+end
