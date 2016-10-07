5050f13bf555e1072fe681e44f13aae80f5632a8
cameroncogan <camycogan@gmail.com>
2016-10-07 15:03:57 -0400

    more hound shit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
  factory :user do
    first_name 'Chris'
    last_name 'Serpa'
-   sequence(:email) { |n| 'cserpa123' + n + '@gmail.com' }
+   sequence(:email) { |n| 'cserpa123' + n.to_s + '@gmail.com' }
    password 'devise'
  end
 end
