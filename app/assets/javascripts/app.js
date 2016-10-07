904e1297d6ac0af52a8abc7d49d5c0d64f4984d6
cserpa <cserpa123@gmail.com>
2016-10-07 15:02:24 -0400

    semicolons

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-  setInterval(updateVotes, 500)
+  setInterval(updateVotes, 500);
 });
 
 let updateVotes = function() {
   let ratingElements = $(".rating");
   ratingElements.each(function(index){
-    let rating = ratingElements[index]
+    let rating = ratingElements[index];
     let rating_id = rating.getAttribute('id');
     $.ajax({
       url: '/votes/' + rating_id,
       contentType: 'application/json'
     })
     .done(total => {
-      rating.innerHTML = total
+      rating.innerHTML = total;
     })
   })
 }
