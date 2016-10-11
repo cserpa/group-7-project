da8e813d8d37688130f07c420ec05c81f8a71196
cserpa <cserpa123@gmail.com>
2016-10-11 16:03:44 -0400

    Stupid mistake

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class FiguresController < ApplicationController
   def index
-    @figures = get_figures
+    @figures = display_figures
     if params[:search]
       @figures = Figure.search(params[:search])
     end
