fa4bf18361af86ca0e1a428f2bd9599d0469645e
cameroncogan <camycogan@gmail.com>
2016-10-12 14:01:31 -0400

    seperated tests to expand coverage

--- spec/features/12_user_uses_nav_bar_spec.rb
+++ spec/features/12_user_uses_nav_bar_spec.rb
@@ -4,40 +4,55 @@ require 'rails_helper'
 feature 'navbar has links to site features' do
   let!(:user) { FactoryGirl.create(:user) }
 
-  scenario 'authenticated user clicks on nav bar links from any page' do
+  scenario 'authenticated user sees navbar on homepage' do
     sign_in user
     visit root_path
-    expect(page).to have_link 'Sign Out'
-    expect(page).to have_link 'Add a Figure'
-    expect(page).to have_link 'Figures'
-    expect(page).to have_link 'Profile'
 
-    click_link 'Profile'
-    visit root_path
-    expect(page).to have_link 'Sign Out'
-    expect(page).to have_link 'Add a Figure'
-    expect(page).to have_link 'Figures'
-    expect(page).to have_link 'Profile'
-
-    click_link 'Add a Figure'
-    visit root_path
     expect(page).to have_link 'Sign Out'
     expect(page).to have_link 'Add a Figure'
     expect(page).to have_link 'Figures'
     expect(page).to have_link 'Profile'
   end
 
-  scenario 'unauthenticated user clicks on nav bar links from any page' do
+  scenario 'authenticated user sees navbar on profile page' do
+    sign_in user
+    visit root_path
+    click_link 'Profile'
+
+    expect(page).to have_link 'Sign Out'
+    expect(page).to have_link 'Add a Figure'
+    expect(page).to have_link 'Figures'
+    expect(page).to have_link 'Profile'
+  end
+
+  scenario 'authenticated user sees navbar on new figure page' do
+    sign_in user
+    visit root_path
+    click_link 'Add a Figure'
+
+    expect(page).to have_link 'Sign Out'
+    expect(page).to have_link 'Add a Figure'
+    expect(page).to have_link 'Figures'
+    expect(page).to have_link 'Profile'
+  end
+
+  scenario 'unauthenticated user clicks on nav bar links from homepage' do
     visit root_path
     expect(page).to have_link 'Sign Up'
     expect(page).to have_link 'Sign In'
     expect(page).to have_link 'Figures'
+  end
 
+  scenario 'unauthenticated user clicks on nav bar links from sign up page' do
+    visit root_path
     click_link 'Sign Up'
     expect(page).to have_link 'Sign Up'
     expect(page).to have_link 'Sign In'
     expect(page).to have_link 'Figures'
+  end
 
+  scenario 'unauthenticated user clicks on nav bar links from sign in page' do
+    visit root_path
     click_link 'Sign In'
     expect(page).to have_link 'Sign Up'
     expect(page).to have_link 'Sign In'
