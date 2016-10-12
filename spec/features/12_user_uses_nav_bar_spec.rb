279c0a2fc471cbdcd837b5d12d4a809717e4d842
cameroncogan <camycogan@gmail.com>
2016-10-12 10:20:34 -0400

    merged with recent master and refactored spec test 10 to use user_ids

--- spec/features/12_user_uses_nav_bar_spec.rb
+++ spec/features/12_user_uses_nav_bar_spec.rb
@@ -7,41 +7,40 @@ feature 'navbar has links to site features' do
   scenario 'authenticated user clicks on nav bar links from any page' do
     sign_in user
     visit root_path
-    expect(page).to have_link "Sign Out"
-    expect(page).to have_link "Add a Figure"
-    expect(page).to have_link "Figures"
-    expect(page).to have_link "Profile"
+    expect(page).to have_link 'Sign Out'
+    expect(page).to have_link 'Add a Figure'
+    expect(page).to have_link 'Figures'
+    expect(page).to have_link 'Profile'
 
-    click_link "Profile"
+    click_link 'Profile'
     visit root_path
-    expect(page).to have_link "Sign Out"
-    expect(page).to have_link "Add a Figure"
-    expect(page).to have_link "Figures"
-    expect(page).to have_link "Profile"
+    expect(page).to have_link 'Sign Out'
+    expect(page).to have_link 'Add a Figure'
+    expect(page).to have_link 'Figures'
+    expect(page).to have_link 'Profile'
 
-    click_link "Add a Figure"
+    click_link 'Add a Figure'
     visit root_path
-    expect(page).to have_link "Sign Out"
-    expect(page).to have_link "Add a Figure"
-    expect(page).to have_link "Figures"
-    expect(page).to have_link "Profile"
+    expect(page).to have_link 'Sign Out'
+    expect(page).to have_link 'Add a Figure'
+    expect(page).to have_link 'Figures'
+    expect(page).to have_link 'Profile'
   end
 
   scenario 'unauthenticated user clicks on nav bar links from any page' do
     visit root_path
-    expect(page).to have_link "Sign Up"
-    expect(page).to have_link "Sign In"
-    expect(page).to have_link "Figures"
+    expect(page).to have_link 'Sign Up'
+    expect(page).to have_link 'Sign In'
+    expect(page).to have_link 'Figures'
 
+    click_link 'Sign Up'
+    expect(page).to have_link 'Sign Up'
+    expect(page).to have_link 'Sign In'
+    expect(page).to have_link 'Figures'
 
-    click_link "Sign Up"
-    expect(page).to have_link "Sign Up"
-    expect(page).to have_link "Sign In"
-    expect(page).to have_link "Figures"
-
-    click_link "Sign In"
-    expect(page).to have_link "Sign Up"
-    expect(page).to have_link "Sign In"
-    expect(page).to have_link "Figures"
+    click_link 'Sign In'
+    expect(page).to have_link 'Sign Up'
+    expect(page).to have_link 'Sign In'
+    expect(page).to have_link 'Figures'
   end
 end
