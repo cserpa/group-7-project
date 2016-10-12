561e89a899ea6427f0b935d7b652cbb2c8b0af68
cameroncogan <camycogan@gmail.com>
2016-10-12 16:55:57 -0400

    another tweak to get codeship to pass

--- spec/features/13_user_can_vote_on_reviews_spec.rb
+++ spec/features/13_user_can_vote_on_reviews_spec.rb
@@ -35,14 +35,14 @@ feature 'user can upvote or downvote a review' do
     visit root_path
   end
 
-  scenario 'user upvotes a review and then changes to downvote', js: true do
+  scenario 'user downvotes a review and tries to downvote it again', js: true do
     sign_in(user)
     visit figure_path(figure)
 
-    click_on 'Upvote'
     click_on 'Downvote'
-    
-    expect(page).to have_content '-1'
+    click_on 'Downvote'
+
+    expect(page).to have_content '0'
     visit root_path
   end
 end
